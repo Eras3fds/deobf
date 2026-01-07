ec5081a27231ac54fcfddf0e938e6f7aa29a7052
xConixyCEO <kaimathemonkey@gmail.com>
2026-01-06 20:13:09 -0500

    Update package.lua

--- package.lua
+++ package.lua
@@ -1,15 +1,13 @@
-{
-  "name": "prometheus-deobfuscator-bot",
-  "version": "1.0.0",
-  "description": "Discord bot for deobfuscating Prometheus-obfuscated Lua scripts",
-  "main": "bot.lua",
-  "dependencies": {
-    "SinisterRectus/discordia": "2.8.4"
+return {
+  name = "prometheus-deobfuscator-bot",
+  version = "1.0.0",
+  description = "Discord bot for deobfuscating Prometheus-obfuscated Lua scripts",
+  main = "bot.lua",
+  dependencies = {
+    "SinisterRectus/discordia"
   },
-  "scripts": {
-    "start": "luajit bot.lua"
-  },
-  "keywords": ["discord", "lua", "deobfuscator", "prometheus"],
-  "author": "",
-  "license": "MIT"
+  files = {
+    "**.lua",
+    "!test*"
+  }
 }
